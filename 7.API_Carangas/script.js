620001dd3c583bd0b19026178faa44a350cfb312
Pablo Garcia <89212003+PabloGarcia48@users.noreply.github.com>
2025-03-18 11:43:36 -0300

    Adicionado método POST e DELETE na API

--- 7.API_Carangas/script.js
+++ 7.API_Carangas/script.js
@@ -12,12 +12,17 @@ async function fetchData() {
         tableBody.innerHTML = '';
 
         cars.reverse().forEach(function(car) {
+            const fuelText = getFuelName(car.gasType);
             
             const row = `
-            <tr data-id="${car.id}">
+            <tr data-id="${car._id}">
                 <td>${car.brand}</td>   
                 <td>${car.name}</td>   
                 <td>${car.price}</td>   
+                <td>${fuelText}</td>
+                <td>
+                <button class="btn btn-danger btn-sm" id="delete-btn" data-id="${car._id}">Deletar</button>
+                </td>
             `;
             tableBody.innerHTML += row;
         })
@@ -29,6 +34,79 @@ async function fetchData() {
         
     }
 
+}
+
+function getFuelName(value) {
+    const fuelTypes = {
+        "0" : "Desconhecido",
+        "1" : "Gasolina",
+        "2" : "Etanol + Gasolina",
+        "3" : "Eletricidade",
+        "4" : "Etanol",
+    };
+    return fuelTypes[value] || " - ";
+}
+
+
+async function addCar() {
+    // event.preventDefault();
+
+    var data = {
+            "brand": "Volvo",
+            "gasType": 2,
+            "name": "XC 60",
+            "price": 150000
+    }
+
+    try {
+        const response = await fetch('https://carangas.herokuapp.com/cars' ,{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+        },
+            body: JSON.stringify(data)
+    });
+
+    console.log("Carro adicionado com sucesso");
+
+    fetchData();
+
+    } catch (error) {
+        console.error(error);
+    }
+
+}
+
+document.getElementById("carForm").addEventListener("submit", addCar())
+
+document.addEventListener('click', function(event) {
+    if (event.target.id.startsWith('delete-btn')) {
+        const carId = event.target.getAttribute('data-id');
+
+        deleteCar(carId);
+    }
+})
+
+async function deleteCar(carId) {
+    try {
+        const response = await fetch(`https://carangas.herokuapp.com/cars/${carId}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error("Erro ao Deletar o carro");
+        }
+
+        console.log("Carro deletado com sucesso!");
+        
+        location.reload()
+
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 
