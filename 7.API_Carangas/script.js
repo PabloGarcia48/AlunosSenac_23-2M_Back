b2dbffce1e857646164dd95984fc30cd040a7da7
Pablo Garcia <89212003+PabloGarcia48@users.noreply.github.com>
2025-03-18 09:12:22 -0300

    Adicionada aula do dia 14/03/25

---
+++ 7.API_Carangas/script.js
@@ -0,0 +1,35 @@
+async function fetchData() {
+    const tableBody = document.getElementById('car-table');
+    tableBody.innerHTML = '<tr><td colspan="5" class="text-center">Carregando carros...</td></tr>'
+
+    try {
+        const response = await fetch('https://carangas.herokuapp.com/cars')
+
+        const data = await response.json();
+        const cars = data;
+        console.log(cars);
+
+        tableBody.innerHTML = '';
+
+        cars.reverse().forEach(function(car) {
+            
+            const row = `
+            <tr data-id="${car.id}">
+                <td>${car.brand}</td>   
+                <td>${car.name}</td>   
+                <td>${car.price}</td>   
+            `;
+            tableBody.innerHTML += row;
+        })
+        
+
+
+
+    } catch (error) {
+        
+    }
+
+}
+
+
+fetchData()
