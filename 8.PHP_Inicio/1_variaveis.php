02541c1f5814114fd4d649b1ce739538ec36f4cf
Pablo Garcia <89212003+PabloGarcia48@users.noreply.github.com>
2025-03-21 10:07:10 -0300

    Arquivos para aula de PHP

---
+++ 8.PHP_Inicio/1_variaveis.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Declare as variáveis $nome, $idade, $altura e $casado com valores fictícios.
+Exiba os valores usando echo.
+Exiba o tipo de cada variável
+*/
+
+
+?>
---
+++ 8.PHP_Inicio/2_matematica_operacoes.php
@@ -0,0 +1,7 @@
+<?php
+/*
+Receba dois números e os armazene em variáveis.
+Exiba a soma, subtração, multiplicação e divisão entre eles.
+*/
+
+?>
